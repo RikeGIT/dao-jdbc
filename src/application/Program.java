1ed03f5c177978807c78627557550e3d5581ab75
Rikelmy <rikecristian@gmail.com>
2025-03-22 09:59:06 -0300

    Implementando o metodo findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,16 +5,15 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 public class Program {
-    public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-
-        Seller seller = new Seller(21, "joão", "joao@gmail.com", new Date(), 3000.0, department);
+    public static void main(String[] args) throws SQLException {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
